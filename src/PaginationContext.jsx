9e671bb1ed52caae90333a22ff17f3a0ce9180c0
Perez Githumbi <codakniggz145@gmail.com>
2024-03-18 19:55:44 +0300

    Implemented Context to persist pagination state once user leaves the given table page

---
+++ src/PaginationContext.jsx
@@ -0,0 +1,97 @@
+import { createContext, useState } from "react";
+
+export const TablePagesContext = createContext({
+  loanBooksTable: {
+    pageIndex: 0,
+    setPageIndex: () => {},
+    pageSize: 8,
+    setPageSize: () => {},
+    sorting: [],
+    setSorting: () => {},
+  },
+  allBooksTable: {
+    pageIndex: 0,
+    setPageIndex: () => {},
+    pageSize: 2,
+    setPageSize: () => {},
+    sorting: [],
+    setSorting: () => {},
+  },
+  returnedBooksTable: {
+    pageIndex: 0,
+    setPageIndex: () => {},
+    pageSize: 6,
+    setPageSize: () => {},
+    sorting: [],
+    setSorting: () => {},
+  },
+  approvalTable: {
+    // approvalTablePageIndex: 0,
+    // approvalTableSetPageIndex: () => {},
+    // approvalTablePageSize: 6,
+    // approvalTableSetPageSize: () => {},
+    // approvalTableSorting: [],
+    // approvalTableSetSorting: () => {},
+  },
+});
+const PaginationContext = ({ children }) => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(8);
+  const [sorting, setSorting] = useState([]);
+
+  const [allBooksPageIndex, allBooksSetPageIndex] = useState(0);
+  const [allBooksPageSize, allBooksSetPageSize] = useState(8);
+  const [allBooksSorting, allBooksSetSorting] = useState([]);
+
+  const [returnedBooksPageIndex, returnedBooksSetPageIndex] = useState(0);
+  const [returnedBooksPageSize, returnedBooksSetPageSize] = useState(8);
+  const [returnedBooksSorting, returnedBooksSetSorting] = useState([]);
+
+  const [approvalTablePageIndex, approvalTableSetPageIndex] = useState(0);
+  const [approvalTablePageSize, approvalTableSetPageSize] = useState(8);
+  const [approvalTableSorting, approvalTableSetSorting] = useState([]);
+
+  return (
+    <TablePagesContext.Provider
+      className="Provider"
+      value={{
+        loanBooksTable: {
+          sorting,
+          pageIndex,
+          pageSize,
+          setPageIndex,
+          setPageSize,
+          setSorting,
+        },
+        allBooksTable: {
+          allBooksPageIndex,
+          allBooksPageSize,
+          allBooksSorting,
+          allBooksSetPageIndex,
+          allBooksSetPageSize,
+          allBooksSetSorting,
+        },
+        returnedBooksTable: {
+          returnedBooksPageIndex,
+          returnedBooksPageSize,
+          returnedBooksSorting,
+          returnedBooksSetPageIndex,
+          returnedBooksSetPageSize,
+          returnedBooksSetSorting,
+        },
+        approvalTable: {
+          approvalTablePageIndex,
+          approvalTablePageSize,
+          approvalTableSorting,
+          approvalTableSetPageIndex,
+          approvalTableSetPageSize,
+          approvalTableSetSorting,
+        },
+      }}
+    >
+      {children}
+    </TablePagesContext.Provider>
+  );
+};
+
+export default PaginationContext;
