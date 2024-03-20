4a4bfea477f4d035d55d22d6f85ebfe583cec846
Perez Githumbi <codakniggz145@gmail.com>
2024-03-20 20:35:48 +0300

    Wrote the general error page

---
+++ src/common/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { useLocation, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const location = useLocation();
+  console.log("Location: ", location);
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>{}</p>
+    </div>
+  );
+}
