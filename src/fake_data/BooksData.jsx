4f418487c8b9560aba923c874e992c9bc756adec
Perez Githumbi <codakniggz145@gmail.com>
2024-03-20 20:38:57 +0300

    Fake data module for developmental purposes

---
+++ src/fake_data/BooksData.jsx
@@ -0,0 +1,28 @@
+const books = [
+  {
+    id: 1,
+    title: "The gods must be craycray",
+    publisher: "pub",
+    category: "cat",
+  },
+  {
+    id: 2,
+    title: "Lorem, ipsum dolor.",
+    publisher: "pub2",
+    category: "cat2",
+  },
+  {
+    id: 3,
+    title: "3book",
+    publisher: "pub3",
+    category: "cat3",
+  },
+  {
+    id: 4,
+    title: "4Lorem, ipsum.",
+    publisher: "pub4",
+    category: "cat4",
+  },
+];
+
+export default books;
