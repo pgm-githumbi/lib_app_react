c060eb9cd692e22048cd45decb10858f938a0859
Perez Githumbi <codakniggz145@gmail.com>
2024-03-27 22:45:31 +0300

    A set of utilities to query category data from the server

---
+++ src/data/CategoryData.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { authConnection } from "./common/server";
+import { apiCategories, apiCategory } from "./ServerResponses";
+import { allBooks, getBook } from "./BooksData";
+import { forIn } from "lodash";
+
+export const getCategories = async () => {
+  const { url, config } = authConnection();
+  const categories = apiCategories(await axios.get(`${url}/category`, config));
+  return categories;
+};
+
+export const getCategory = async (category_id) => {
+  const { url, config } = authConnection();
+  const category = await axios.get(`${url}/category/${category_id}`, config);
+  return apiCategory(category);
+};
+
+export const getCategoriesWithMostBooks = async () => {
+  const category_ids = {};
+  const books = await allBooks();
+  books.map((book) => {
+    category_ids[book.category_id] = (category_ids[book.category_id] || 0) + 1;
+  });
+  const categoryIdList = [];
+  const topCategories = [];
+  forIn(category_ids, (bookCount, category_id, categoryObj) => {
+    categoryIdList.push(category_id);
+  });
+  //Sort the categoryIds
+  categoryIdList.sort((catId1, catId2) => {
+    return category_ids[catId1] - category_ids[catId2];
+  });
+  return await Promise.all(
+    categoryIdList
+      .slice(Math.min(categoryIdList.length, 5), categoryIdList.length)
+      .reverse()
+      .map(async (catId) => {
+        return await getCategory(catId);
+      })
+  );
+};
