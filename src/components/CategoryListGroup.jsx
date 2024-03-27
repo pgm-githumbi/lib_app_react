35a6543fa90ad53e1dc9a7d42adc00ca2d321194
Perez Githumbi <codakniggz145@gmail.com>
2024-03-27 22:47:10 +0300

    A listgroup component to allow the user to filter the books by their categories.

---
+++ src/components/CategoryListGroup.jsx
@@ -0,0 +1,64 @@
+import { useContext, useEffect, useState } from "react";
+import CommonListGroup from "../common/CommonListGroup";
+import { getCategoriesWithMostBooks } from "../data/CategoryData";
+import { ListGroupContexts } from "../contexts/ListGroupContexts";
+import { useQuery } from "react-query";
+const defaultSidebarLinks = [
+  {
+    visibleName: "All",
+    link: "",
+    parentPath: "/home/books",
+    activeOn: ["", "all"],
+  },
+];
+
+const createCategoryLink = ({ category_name, id }) => {
+  return {
+    visibleName: category_name,
+    link: `${id}`,
+    parentPath: "/home/books",
+    activeOn: [`${id}`],
+  };
+};
+const CategoryListGroup = () => {
+  const {
+    data: categories,
+    isError,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: ["categories with most books"],
+    queryFn: getCategoriesWithMostBooks,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="list-group py-4">
+        <div className="list-group-item text-info" role="alert">
+          Loading...
+        </div>
+      </div>
+    );
+  }
+
+  if (isError)
+    return (
+      <div className="list-group py-4">
+        <div className="list-group-item text-danger" role="alert">
+          {" "}
+          An error occurred
+        </div>
+      </div>
+    );
+
+  if (isSuccess) {
+    const categoryLinks = categories.map((category) =>
+      createCategoryLink(category)
+    );
+    return (
+      <CommonListGroup args={[...defaultSidebarLinks, ...categoryLinks]} />
+    );
+  }
+};
+
+export default CategoryListGroup;
