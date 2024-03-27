340b10e51613e9050f5313d016eb326e15cec8b2
Perez Githumbi <codakniggz145@gmail.com>
2024-03-27 20:44:35 +0300

    Component with visuals for the header of the signup page

---
+++ src/components/SignupHeader.jsx
@@ -0,0 +1,12 @@
+const SignupHeader = () => {
+  return (
+    <div className="row">
+      <div role="header" className="jumbotron text-center">
+        <h1 className="display-3">Library App</h1>
+        <small className="lead">Create an account</small>
+      </div>
+    </div>
+  );
+};
+
+export default SignupHeader;
