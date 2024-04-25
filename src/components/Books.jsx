d908fa76ae199df337aacbcbab2e0a09269996ab
Perez Githumbi <codakniggz145@gmail.com>
2024-04-25 20:59:05 +0300

    Added links to show more details of a given book on click.

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -15,7 +15,13 @@ import { TablePagesContext } from "../PaginationContext";
 import { AllBooksContext } from "../contexts/AllBooksContext";
 import CategoryListGroup from "./CategoryListGroup";
 import BooksTable from "./BooksTable";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import {
+  Link,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import ErrorPage from "../common/ErrorPage";
 import { getUsername } from "../data/Auth";
 import { loadingContext } from "../contexts/LoadingContext";
@@ -23,16 +29,53 @@ import { useMutation, useQuery, useQueryClient } from "react-query";
 import { set } from "lodash";
 import BorrowBook from "./BorrowBook";
 
+const currentLocation = "home/books";
 const columnHelper = createColumnHelper();
 const columns = [
-  columnHelper.accessor("id", {
-    cell: (info) => info.getValue(),
+  // columnHelper.accessor("id", {
+  //   cell: (info) => info.getValue(),
+  // }),
+
+  columnHelper.accessor("image", {
+    header: "",
+    cell: (info) => {
+      const book = info.cell.row.original;
+
+      return (
+        <div
+          className="d-flex flex-grow-1 justify-content-center align-items-center"
+          style={{ overflow: "hidden", width: 50, height: 100 }}
+        >
+          <Link to={`one_book/${book?.id}`} state={{ book }}>
+            <img
+              className="img-responsive py-1 mb-3"
+              style={{ width: 50, height: "auto", display: "block" }}
+              src={info.getValue()}
+              alt="coverpage"
+            />
+          </Link>
+        </div>
+      );
+    },
   }),
   columnHelper.accessor("name", {
-    header: "title",
-    cell: (info) => info.getValue(),
+    header: "Title",
+    cell: (info) => {
+      const book = info.cell.row.original;
+      return (
+        <Link to={`one_book/${book?.id}`} state={{ book }}>
+          {info.getValue()}
+        </Link>
+      );
+    },
   }),
   columnHelper.accessor("publisher", {
+    header: (
+      <span>
+        Publisher
+        <i className="fa-solid fa-briefcase px-2"></i>
+      </span>
+    ),
     cell: (info) => info.getValue(),
   }),
   columnHelper.accessor("category", {
@@ -81,9 +124,9 @@ const Books = () => {
   return (
     <div className="container-fluid text-center">
       <div className="row py-5">
-        <div className="col-md">
+        <div className="col-12 col-md-9">
           <div className="jumbotron">
-            <h1 className="display-4">Hello, {getUsername() || "user"}!</h1>
+            <h1 className="display-4">Hello👋, {getUsername() || "user"}!</h1>
             <p className="lead">This is a list of all available books.</p>
             <p></p>
           </div>
