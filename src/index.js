949835fbfb77ad8e7c4bfca26bd177be505a089f
Perez Githumbi <codakniggz145@gmail.com>
2024-05-21 19:53:32 +0300

    using hash router instead of browser router for gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
@@ -15,9 +15,9 @@ import ErrorPage from "./common/ErrorPage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Main />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
